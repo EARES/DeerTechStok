dc9cfaf8fd62e2396401cde656bd2bd692f58dc9
EMRE <nonemre@gmail.com>
2018-08-01 10:57:12 +0300

    Initial commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCuGm7duaUpWC7vP_hljZAUtPj6yU9SU1Y",
+    authDomain: "deertechstok-a8e85.firebaseapp.com",
+    databaseURL: "https://deertechstok-a8e85.firebaseio.com",
+    projectId: "deertechstok-a8e85",
+    storageBucket: "deertechstok-a8e85.appspot.com",
+    messagingSenderId: "28847385751"
+}
